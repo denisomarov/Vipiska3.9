df5eacb0a0cf82ccf6a3f45849f74f0cdda12c2d
Denis <omarov_denis@mail.ru>
2022-07-03 20:41:32 +0500

    Procfile change version

--- interface.py
+++ interface.py
@@ -34,7 +34,7 @@ def index():
 
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
-    return send_file(filename)
+    return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
